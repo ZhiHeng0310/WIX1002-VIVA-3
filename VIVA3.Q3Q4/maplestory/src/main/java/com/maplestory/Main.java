3de0e38c7b6995b32ca908300b185ee4b17ecc91
zerng <zerngx@gmail.com>
2025-12-30 19:20:55 +0800

    Initial commit Q3

---
+++ VIVA3.Q3Q4/maplestory/src/main/java/com/maplestory/Main.java
@@ -0,0 +1,10 @@
+package com.maplestory;
+
+import main.java.com.maplestory.MagicShield;
+
+public class Main {
+    public static void main(String[] args) {
+        MagicShield m1 = new MagicShield();
+        System.out.println(m1);
+    }
+}
---
+++ maplestory/src/main/java/com/maplestory/MagicShieldTest.java
@@ -0,0 +1,9 @@
+package com.maplestory;
+
+public class MagicShieldTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
